693576a650261d7b0f82d6a3570b2a6e81529fda
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-12 15:17:47 -0600

    feat: ZoomIn y ZoomOut

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   selector: 'app-zoom-range-page',
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css'
 })
-export class ZoomRangePageComponent {
+export class ZoomRangePageComponent implements AfterViewInit {
+
+  @ViewChild('map') divMap?:ElementRef;
+
+  ngAfterViewInit(): void {
+
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 
 }
