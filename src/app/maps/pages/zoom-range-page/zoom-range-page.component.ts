bbe5349012b65e0a6863116cbea973ab1b6f7c5c
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-12 16:59:01 -0600

    feat: Control the level of zoom

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -9,18 +9,48 @@ import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 })
 export class ZoomRangePageComponent implements AfterViewInit {
 
+
   @ViewChild('map') divMap?:ElementRef;
+
+  public zoom:number = 10;
+  public map?:Map;
 
   ngAfterViewInit(): void {
 
     if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+    this.mapListener();
+  }
+
+  mapListener(){
+    if(!this.map) throw 'Mapa no inicializado';
+    this.map.on('zoom',(ev) => {
+      this.zoom = this.map!.getZoom();
+    })
+    this.map.on('zoomend',(ev) => {
+      if(this.map!.getZoom() < 18) return;
+      this.map!.zoomTo(18);
+      // this.zoom = this.map!.getZoom();
+    })
+  }
+
+  zoomIn(){
+    this.map?.zoomIn();
+  }
+
+  zoomOut(){
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value:string){
+    this.zoom = Number(value);
+    this.map?.zoomTo( this.zoom);
   }
 
 }
