0c2f5f59994fe20bc2c09fe75cb0174515344386
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-12 20:00:08 -0600

    feat: Markers on the map

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { LngLat, Map, Marker } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
 
 @Component({
   selector: 'app-markers-page',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrl: './markers-page.component.css'
 })
 export class MarkersPageComponent {
+
+  @ViewChild('map') divMap?:ElementRef;
+
+  public zoom:number = 10;
+  public map?:Map;
+  public currentLngLat:LngLat = new LngLat(-103.29396589339024, 20.634026187927958);
+
+  ngAfterViewInit(): void {
+
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+    });
+    // this.mapListener();
+
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Fernanda Herrera'
+
+    // const marker = new Marker({ color: 'red', element: markerHtml })
+    //   .setLngLat(this.currentLngLat)
+    //   .addTo(this.map)
+  }
 
 }
