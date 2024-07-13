a292b5b00731ba13fb7d6f6b7517b3d1ff033636
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-12 19:44:16 -0600

    feat: Obtain the central coordinates of the map

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,19 +1,20 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 
-import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import { LngLat, Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   selector: 'app-zoom-range-page',
   templateUrl: './zoom-range-page.component.html',
   styleUrl: './zoom-range-page.component.css'
 })
-export class ZoomRangePageComponent implements AfterViewInit {
+export class ZoomRangePageComponent implements AfterViewInit,OnDestroy {
 
 
   @ViewChild('map') divMap?:ElementRef;
 
   public zoom:number = 10;
   public map?:Map;
+  public currentLngLat:LngLat = new LngLat(-103.29396589339024, 20.634026187927958);
 
   ngAfterViewInit(): void {
 
@@ -22,10 +23,14 @@ export class ZoomRangePageComponent implements AfterViewInit {
     this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
     this.mapListener();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
   }
 
   mapListener(){
@@ -37,6 +42,9 @@ export class ZoomRangePageComponent implements AfterViewInit {
       if(this.map!.getZoom() < 18) return;
       this.map!.zoomTo(18);
       // this.zoom = this.map!.getZoom();
+    })
+    this.map.on('move',()=>{
+      this.currentLngLat = this.map!.getCenter();
     })
   }
 
