887f803997faf6cedfa4b2a1ea8fcd0d88295f09
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-12 20:10:39 -0600

    feat: Dynamic markers

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -36,4 +36,21 @@ export class MarkersPageComponent {
     //   .addTo(this.map)
   }
 
+  createMarker(){
+    if( !this.map ) return;
+
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lngLat = this.map.getCenter();
+    
+    this.addMarker( lngLat,color );
+  }
+
+  addMarker( lngLat:LngLat,color:string ){
+    if( !this.map ) return;
+
+    const marker = new Marker({ color: color, draggable: true})
+      .setLngLat(lngLat)
+      .addTo(this.map)
+  }
+
 }
