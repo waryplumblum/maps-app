1d303700fe88f560ad1c3a3b0213d76004e18fc5
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-15 14:03:52 -0600

    feat: standalone components

---
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CounterAloneComponent } from "../../components/counter-alone/counter-alone.component";
+
+@Component({
+  selector: 'alone-page',
+  standalone: true,
+  imports: [CounterAloneComponent],
+  templateUrl: './alone-page.component.html',
+  styleUrl: './alone-page.component.css'
+})
+export class AlonePageComponent {
+
+}
