c973f6ebf5b67075236c8ae10d31fb7a39bee101
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-07-15 23:37:18 -0600

    feat: traditional component to a standalone componenet

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CounterAloneComponent } from "../../components/counter-alone/counter-alone.component";
+import { SideMenuComponent } from "../../components/side-menu/side-menu.component";
 
 @Component({
   selector: 'alone-page',
   standalone: true,
-  imports: [CounterAloneComponent],
+  imports: [CounterAloneComponent, AlonePageComponent, SideMenuComponent],
   templateUrl: './alone-page.component.html',
   styleUrl: './alone-page.component.css'
 })
